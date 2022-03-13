3c493795a414d10efddd8bbb9739f956d9fa4512
tougaj <sashamolodec@gmail.com>
2022-03-13 18:41:18 +0200

    Реализовано конвертирование каталога.

--- kmz2json/js/convert.js
+++ kmz2json/js/convert.js
@@ -8,19 +8,30 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 const parseKMZ = require('parse2-kmz');
-const fs = require('fs');
+const fs_1 = __importDefault(require("fs"));
 const INPUT_PATH = './input/';
 const OUTPUT_PATH = './output/';
 const convertKmzToJson = (baseFilename) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const kmz = yield parseKMZ.toJson(`${INPUT_PATH}${baseFilename}.kmz`);
+        const kmz = yield parseKMZ.toJson(`${INPUT_PATH}${baseFilename}`);
         const json = JSON.stringify(kmz, undefined, 2);
-        fs.writeFileSync(`${OUTPUT_PATH}${baseFilename}.json`, json);
-        console.log('Done');
+        fs_1.default.writeFileSync(`${OUTPUT_PATH}${baseFilename}.json`, json);
+        console.log(`${baseFilename} converted`);
+        return true;
     }
     catch (error) {
-        console.error(error);
+        console.error(`Error converting file ${baseFilename}: ${error}`);
     }
+    return false;
 });
-convertKmzToJson('Обстановка');
+const convertFiles = () => __awaiter(void 0, void 0, void 0, function* () {
+    const files = fs_1.default.readdirSync(INPUT_PATH).filter((file) => file.endsWith('.kmz'));
+    yield Promise.all(files.map((file) => convertKmzToJson(file)));
+    console.log('All files converted');
+});
+convertFiles();
--- kmz2json/src/js/convert.ts
+++ kmz2json/src/js/convert.ts
@@ -1,17 +1,28 @@
 const parseKMZ = require('parse2-kmz');
-const fs = require('fs');
+import fs from 'fs';
 const INPUT_PATH = './input/';
 const OUTPUT_PATH = './output/';
 
 const convertKmzToJson = async (baseFilename: string) => {
 	try {
-		const kmz = await parseKMZ.toJson(`${INPUT_PATH}${baseFilename}.kmz`);
+		const kmz = await parseKMZ.toJson(`${INPUT_PATH}${baseFilename}`);
 		const json = JSON.stringify(kmz, undefined, 2);
 		fs.writeFileSync(`${OUTPUT_PATH}${baseFilename}.json`, json);
-		console.log('Done');
+		console.log(`${baseFilename} converted`);
+		return true;
 	} catch (error) {
-		console.error(error);
+		console.error(`Error converting file ${baseFilename}: ${error}`);
 	}
+	return false;
 };
 
-convertKmzToJson('Обстановка');
+const convertFiles = async () => {
+	const files = fs.readdirSync(INPUT_PATH).filter((file) => file.endsWith('.kmz'));
+
+	await Promise.all(files.map((file) => convertKmzToJson(file)));
+
+	console.log('All files converted');
+};
+
+convertFiles();
+// convertKmzToJson('Обстановка');
