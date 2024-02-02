904458da502dcb731f81f366c26213b2f6c7e7de
tougaj <sashamolodec@gmail.com>
2024-02-02 15:24:37 +0200

    Додано приклад Watch File

---
+++ fsWatch/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+require('log-timestamp');
+var watch = require('node-watch');
+
+const filePath = './test.txt';
+
+const watcher = watch(filePath, { recursive: false });
+watcher.on('change', function(evt, name) {
+	console.log('✅ %s changed.', name);
+});
+watcher.on('error', (err)=>{
+	console.log('❗', err);
+})
+// watch(filePath, { recursive: false }, function(evt, name) {
+// 	console.log('%s changed.', name);
+// });
+
+// Тут множинне спрацювання. І якщо використовувати цей метод, то треба робити debounce.
+// const fs = require('fs');
+// fs.watch(filePath, (event, filename) => {
+//   if (filename) {
+//     console.log(`${filename} file Changed`);
+//   }
+// });
+
+
