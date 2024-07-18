4086089e08b7ba6419bd8ec6387cb8a78c08dd0b
tougaj <sashamolodec@gmail.com>
2024-07-18 18:48:34 +0300

    Налаштування скриптів

--- deepFace/scripts/createSolrSchema.sh
+++ deepFace/scripts/createSolrSchema.sh
@@ -8,35 +8,38 @@
 # Фактично в результаті даної команди створюється файл ./server/solr/faces/conf/configoverlay.json з наступним вмістом:
 # {"userProps":{"update.autoCreateFields":"false"}
 
-# curl \
-# 	-X POST \
-# 	-H 'Content-type:application/json' \
-# 	--data-binary '{"delete-field-type":{ "name":"face_vector" }}' \
-# 	http://localhost:8983/solr/faces/schema
-# curl \
-# 	-X POST \
-# 	-H 'Content-type:application/json' \
-# 	--data-binary '{
-# 		"add-field-type" : {
-# 		"name":"face_vector",
-# 		"class":"solr.DenseVectorField",
-# 		"vectorDimension":"4096",
-# 		"similarityFunction":"euclidean",
-# 		"knnAlgorithm":"hnsw",
-# 		"hnswMaxConnections":"512",
-# 		"hnswBeamWidth":"1024"}}' \
-# 	http://localhost:8983/solr/faces/schema
-
-# curl \
-# 	-X POST \
-# 	-H 'Content-type:application/json' \
-# 	--data-binary '{"delete-field": {"name":"face"}}' \
-# 	http://localhost:8983/solr/faces/schema
-# curl \
-# 	-X POST \
-# 	-H 'Content-type:application/json' \
-# 	--data-binary '{"add-field": {"name":"face", "type":"face_vector", "stored":true, "indexed": true}}' \
-# 	http://localhost:8983/solr/faces/schema
-
-
 # curl -X POST -H 'Content-type:application/json' --data-binary '{"delete": {"query": "*:*"}}' http://localhost:8983/solr/faces/update?commit=true
+
+curl \
+	-X POST \
+	-H 'Content-type:application/json' \
+	--data-binary '{"delete-field": {"name":"face"}}' \
+	http://localhost:8983/solr/faces/schema
+
+curl \
+	-X POST \
+	-H 'Content-type:application/json' \
+	--data-binary '{"delete-field-type":{ "name":"face_vector" }}' \
+	http://localhost:8983/solr/faces/schema
+
+curl \
+	-X POST \
+	-H 'Content-type:application/json' \
+	--data-binary '{
+		"add-field-type" : {
+		"name":"face_vector",
+		"class":"solr.DenseVectorField",
+		"vectorDimension":"4096",
+		"similarityFunction":"euclidean",
+		"knnAlgorithm":"hnsw",
+		"hnswMaxConnections":"512",
+		"hnswBeamWidth":"3200"}}' \
+	http://localhost:8983/solr/faces/schema
+
+curl \
+	-X POST \
+	-H 'Content-type:application/json' \
+	--data-binary '{"add-field": {"name":"face", "type":"face_vector", "stored":true, "indexed": true}}' \
+	http://localhost:8983/solr/faces/schema
+
+
