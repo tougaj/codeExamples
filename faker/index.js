f5379fb21a0c4df556d268e993b71bdf63cc004e
tougaj <sashamolodec@gmail.com>
2024-09-04 10:30:17 +0300

    Додано приклад генерації випадкових зображень

--- faker/index.js
+++ faker/index.js
@@ -1,23 +1,26 @@
-import { faker, fakerUK, fakerDE, fakerRU } from '@faker-js/faker';
+import { faker, fakerDE, fakerRU, fakerUK } from '@faker-js/faker';
 
 const BASE_COUNT = 2;
 
 const generate = (fn, count = BASE_COUNT) => {
 	for (var i = 0; i < count; i++) {
-		console.log(fn())
-	}	
-	console.log('----------')
-}
+		console.log(fn());
+	}
+	console.log('----------');
+};
 
-const company = f => () => f.company.name()+' '+f.company.catchPhrase();
+const company = (f) => () => f.company.name() + ' ' + f.company.catchPhrase();
 
-generate(fakerUK.person.fullName, BASE_COUNT*2);
+generate(fakerUK.person.fullName, BASE_COUNT * 2);
 generate(fakerRU.person.fullName);
 generate(faker.person.fullName);
 generate(fakerDE.person.fullName);
 
-generate(fakerUK.company.name, BASE_COUNT*2);
+generate(fakerUK.company.name, BASE_COUNT * 2);
 generate(fakerRU.company.name);
 generate(company(faker));
 generate(fakerDE.company.name);
 
+generate(() => {
+	return faker.image.urlLoremFlickr({ category: 'people', width: 128, height: 128 });
+}, 5);
