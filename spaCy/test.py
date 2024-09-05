afa476c8e4393dbcf93176e16c744993b1e7e8dc
tougaj <sashamolodec@gmail.com>
2024-09-05 16:56:29 +0300

    Додані приклади

--- spaCy/test.py
+++ spaCy/test.py
@@ -17,7 +17,7 @@ tic = time()
 doc=nlp("Після удару РФ балістичними ракетами по місту Полтава міністр закордонних справ України Дмитро Кулеба закликав партнерів прискорити доставку новітніх систем ППО і посилювати здатність України протидіяти балістичним атакам ворога.")
 toc = time()
 for token in doc:
-    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop)
+    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop, token.morph)
 print("--------------------------------------------------")
 for ent in doc.ents:
     print(ent.text, ent.start_char, ent.end_char, ent.label_)
