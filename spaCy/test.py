0bf03592858e2dc85904351624056bcc02a98cde
tougaj <sashamolodec@gmail.com>
2024-09-08 15:57:18 +0300

    Додано приклади

--- spaCy/test.py
+++ spaCy/test.py
@@ -7,21 +7,49 @@ import uk_core_news_lg
 # import ru_core_news_lg
 # import uk_core_news_trf
 # from spacy import displacy
+from spacy.symbols import nsubj, VERB
 
 # nlp = spacy.load("uk_core_news_lg")
 # nlp = uk_core_news_sm.load()
 nlp = uk_core_news_lg.load()
+# nlp = uk_core_news_lg.load(disable=["parser"])
 # nlp = ru_core_news_lg.load()
 # nlp = uk_core_news_trf.load()
 tic = time()
 doc=nlp("Після удару РФ балістичними ракетами по місту Полтава міністр закордонних справ України Дмитро Кулеба закликав партнерів прискорити доставку новітніх систем ППО і посилювати здатність України протидіяти балістичним атакам ворога.")
 toc = time()
 for token in doc:
-    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop, token.morph)
+    # print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop, token.morph)
+    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop, token.head.text, [child for child in token.children])
+    if token.n_lefts or token.n_rights:
+        print('⛓️‍', [t.text for t in token.lefts], token.text, [t.text for t in token.rights])
 print("--------------------------------------------------")
+
+# root = [token for token in doc if token.head == token][0]
+# subject = list(root.lefts)[0]
+# for descendant in subject.subtree:
+#     assert subject is descendant or subject.is_ancestor(descendant)
+#     print(descendant.text, descendant.dep_, descendant.n_lefts,
+#             descendant.n_rights,
+#             [ancestor.text for ancestor in descendant.ancestors])
+# print("--------------------------------------------------")
+
 for ent in doc.ents:
-    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+    print(ent.text, ent.lemma_, ent.start_char, ent.end_char, ent.label_)
 print("--------------------------------------------------")
+
+# The 'noun_chunks' syntax iterator is not implemented for language 'uk'.
+# for chunk in doc.noun_chunks:
+#     print(chunk.text, chunk.root.text, chunk.root.dep_, chunk.root.head.text)
+
+# Finding a verb with a subject from below — good
+# verbs = set()
+# for possible_subject in doc:
+#     if possible_subject.dep == nsubj and possible_subject.head.pos == VERB:
+#         verbs.add(possible_subject.head)
+# print(verbs)
+# print("--------------------------------------------------")
+
 
 # displacy.serve(doc, style="dep")
 # displacy.serve(doc, style="ent", port=5001)
