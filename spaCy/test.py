c57dbe8f0a6793eaea7f3d928b5ddfa771613388
tougaj <sashamolodec@gmail.com>
2024-09-10 09:30:13 +0300

    Рефакторинг коду

--- spaCy/test.py
+++ spaCy/test.py
@@ -5,13 +5,66 @@ from time import time
 
 # import uk_core_news_sm
 import uk_core_news_lg
-from spacy.lang.char_classes import (ALPHA, ALPHA_LOWER, ALPHA_UPPER,
-                                     CONCAT_QUOTES, LIST_ELLIPSES, LIST_ICONS)
 # import ru_core_news_lg
 # import uk_core_news_trf
 # from spacy import displacy
-from spacy.symbols import VERB, nsubj
+from spacy.lang.char_classes import (ALPHA, ALPHA_LOWER, ALPHA_UPPER,
+                                     CONCAT_QUOTES, LIST_ELLIPSES, LIST_ICONS)
+# from spacy.symbols import VERB, nsubj
 from spacy.util import compile_infix_regex
+
+
+def get_source_text():
+    content = ''
+    with open('local.text.txt', 'r', encoding='utf-8') as file:
+        content = file.read()
+    return content
+
+
+def adjust_tokenizer(nlp):
+    # If you do not want the tokenizer to split on hyphens between letters, you can modify the existing infix definition from lang/punctuation.py
+    # Modify tokenizer infix patterns
+    infixes = (
+        LIST_ELLIPSES
+        + LIST_ICONS
+        + [
+            r"(?<=[0-9])[+\\-\\*^](?=[0-9-])",
+            r"(?<=[{al}{q}])\\.(?=[{au}{q}])".format(
+                al=ALPHA_LOWER, au=ALPHA_UPPER, q=CONCAT_QUOTES
+            ),
+            r"(?<=[{a}]),(?=[{a}])".format(a=ALPHA),
+            # ✅ Commented out regex that splits on hyphens between letters:
+            # r"(?<=[{a}])(?:{h})(?=[{a}])".format(a=ALPHA, h=HYPHENS),
+            r"(?<=[{a}0-9])[:<>=/](?=[{a}])".format(a=ALPHA),
+        ]
+    )
+    infix_re = compile_infix_regex(infixes)
+    nlp.tokenizer.infix_finditer = infix_re.finditer
+    # For an overview of the default regular expressions, see lang/punctuation.py and language-specific definitions such as https://github.com/explosion/spaCy/blob/master/spacy/lang/en/punctuation.py
+
+
+def print_sentences(doc):
+    for sent in doc.sents:
+        print(sent.text)
+    print("--------------------------------------------------")
+
+
+def print_tokens(doc, with_lr=True):
+    for token in doc:
+        # print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop, token.morph)
+        print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_,
+              token.is_alpha, token.is_stop, token.head.text, [child for child in token.children])
+        if with_lr and (token.n_lefts or token.n_rights):
+            print('⛓️‍', [t.text for t in token.lefts],
+                  token.text, [t.text for t in token.rights])
+    print("--------------------------------------------------")
+
+
+def print_entities(doc):
+    for ent in doc.ents:
+        print(ent.text, ent.lemma_, ent.start_char, ent.end_char, ent.label_)
+    print("--------------------------------------------------")
+
 
 # nlp = spacy.load("uk_core_news_lg")
 # nlp = uk_core_news_sm.load()
@@ -20,57 +73,26 @@ nlp = uk_core_news_lg.load()
 # nlp = ru_core_news_lg.load()
 # nlp = uk_core_news_trf.load()
 
-
-# If you do not want the tokenizer to split on hyphens between letters, you can modify the existing infix definition from lang/punctuation.py
-
-# Modify tokenizer infix patterns
-infixes = (
-    LIST_ELLIPSES
-    + LIST_ICONS
-    + [
-        r"(?<=[0-9])[+\\-\\*^](?=[0-9-])",
-        r"(?<=[{al}{q}])\\.(?=[{au}{q}])".format(
-            al=ALPHA_LOWER, au=ALPHA_UPPER, q=CONCAT_QUOTES
-        ),
-        r"(?<=[{a}]),(?=[{a}])".format(a=ALPHA),
-        # ✅ Commented out regex that splits on hyphens between letters:
-        # r"(?<=[{a}])(?:{h})(?=[{a}])".format(a=ALPHA, h=HYPHENS),
-        r"(?<=[{a}0-9])[:<>=/](?=[{a}])".format(a=ALPHA),
-    ]
-)
-infix_re = compile_infix_regex(infixes)
-nlp.tokenizer.infix_finditer = infix_re.finditer
-# For an overview of the default regular expressions, see lang/punctuation.py and language-specific definitions such as https://github.com/explosion/spaCy/blob/master/spacy/lang/en/punctuation.py
+adjust_tokenizer(nlp)
 
 tic = time()
-doc = nlp("Після удару РФ балістичними ракетами по місту Полтаві міністр закордонних справ України Дмитро Кулеба закликав партнерів прискорити доставку новітніх систем ППО і посилювати здатність України протидіяти балістичним атакам ворога.")
+doc = nlp(get_source_text())
 toc = time()
 
-for sent in doc.sents:
-    print(sent.text)
-print("--------------------------------------------------")
+print_sentences(doc)
+print_tokens(doc, False)
+print_entities(doc)
 
-for token in doc:
-    # print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop, token.morph)
-    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_,
-          token.is_alpha, token.is_stop, token.head.text, [child for child in token.children])
-    if token.n_lefts or token.n_rights:
-        print('⛓️‍', [t.text for t in token.lefts],
-              token.text, [t.text for t in token.rights])
-print("--------------------------------------------------")
 
 # root = [token for token in doc if token.head == token][0]
 # subject = list(root.lefts)[0]
 # for descendant in subject.subtree:
 #     assert subject is descendant or subject.is_ancestor(descendant)
 #     print(descendant.text, descendant.dep_, descendant.n_lefts,
-#             descendant.n_rights,
-#             [ancestor.text for ancestor in descendant.ancestors])
+#           descendant.n_rights,
+#           [ancestor.text for ancestor in descendant.ancestors])
 # print("--------------------------------------------------")
 
-for ent in doc.ents:
-    print(ent.text, ent.lemma_, ent.start_char, ent.end_char, ent.label_)
-print("--------------------------------------------------")
 
 # The 'noun_chunks' syntax iterator is not implemented for language 'uk'.
 # for chunk in doc.noun_chunks:
