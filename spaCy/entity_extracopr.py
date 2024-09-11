e0e9a7cc9254d12fe00174cd3e629a7dc33c274a
tougaj <sashamolodec@gmail.com>
2024-09-11 15:41:59 +0300

    Приклади

--- spaCy/entity_extracopr.py
+++ spaCy/entity_extracopr.py
@@ -3,50 +3,50 @@ import spacy
 # from spacy.lang.uk import Ukrainian
 
 nlp = spacy.load("uk_core_news_lg")
-ruler = nlp.add_pipe("entity_ruler", before="ner")
+# ruler = nlp.add_pipe("entity_ruler", before="ner")
 # ruler = nlp.add_pipe("entity_ruler")
-patterns = [
-    {
-        "label": "RRR",
-        "pattern": [
-            {"LEMMA": "івано"},
-            {"TEXT": "-"},
-            {"LEMMA": "франківськ"},
+# patterns = [
+#     {
+#         "label": "RRR",
+#         "pattern": [
+#             {"LEMMA": "івано"},
+#             {"TEXT": "-"},
+#             {"LEMMA": "франківськ"},
 
-            # {"ENT_TYPE": "LOC"},
-            # {"DEP": "amod"},
-            # {"DEP": "nmod"},
-            # {"ENT_TYPE": "LOC"},
-            # {"LEMMA": "вирушити"}
+#             # {"ENT_TYPE": "LOC"},
+#             # {"DEP": "amod"},
+#             # {"DEP": "nmod"},
+#             # {"ENT_TYPE": "LOC"},
+#             # {"LEMMA": "вирушити"}
 
-            # {"POS": "PROPN", "OP": "?"},
-            # {"POS": "PROPN"},
-            # {"TEXT": "-"},
-            # {"POS": "PROPN"}
-        ]
-        # "label": "RRR",
-        # "pattern": [
-        #     {"LEMMA": "оборонний"},
-        #     {"LEMMA": "споруда"}
-        # ]
-    },
-    {
-        "label": "PER",
-        "pattern": [
-            {"DEP": "flat:title", "OP": "?"},
-            {"DEP": "flat:name"},
-            {"TEXT": "-"},
-            {"DEP": "flat:name"}
-        ]
-    },
-]
+#             # {"POS": "PROPN", "OP": "?"},
+#             # {"POS": "PROPN"},
+#             # {"TEXT": "-"},
+#             # {"POS": "PROPN"}
+#         ]
+#         # "label": "RRR",
+#         # "pattern": [
+#         #     {"LEMMA": "оборонний"},
+#         #     {"LEMMA": "споруда"}
+#         # ]
+#     },
+#     {
+#         "label": "PER",
+#         "pattern": [
+#             {"DEP": "flat:title", "OP": "?"},
+#             {"DEP": "flat:name"},
+#             {"TEXT": "-"},
+#             {"DEP": "flat:name"}
+#         ]
+#     },
+# ]
 # patterns = [{"label": "LOC", "pattern": [
 #     {"LEMMA": "оборонний"}, {"LEMMA": "споруда"}]}]
 # ruler.add_patterns(patterns)
 # [{"label": "ORG", "pattern": "Apple"}]
 #             {"label": "GPE", "pattern": [{"LOWER": "san"}, {"LOWER": "francisco"}]}]
-ruler.add_patterns(patterns)
-doc = nlp('Група таких-собі комунальників з Івано-Франківська вирушила на Донеччину для будівництва оборонних споруд. Співробітників підприємства Водоканал-Сервіс розпитають наши кореспонденти Жанна Дутчак-Малиновська та Степан Бобров.')
+# ruler.add_patterns(patterns)
+doc = nlp('Група таких-собі комунальників з Івано-Франківська вирушила на Донеччину для будівництва оборонних споруд. Про співробітників підприємства Водоканал-Сервіс розпитаємо в наших кореспондентів Жанни Дутчак-Малиновської та Степана Боброва.')
 
 for token in doc:
     print('🔷', token.text, token.lemma_, token.pos_, token.dep_, token.shape_, token.is_stop,
