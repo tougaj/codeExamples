2b597c83636cb489f713c366304a8a6f566b178f
tougaj <sashamolodec@gmail.com>
2024-10-11 14:25:17 +0300

    Удосконалення запиту

--- transformers/translate/inference_API.py
+++ transformers/translate/inference_API.py
@@ -14,7 +14,8 @@ if API_TOKEN is None:
 
 API_URL = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-en-uk"
 headers = {"Authorization": f"Bearer {API_TOKEN}",
-           "Content-Type": "application/json", }
+           "Content-Type": "application/json",
+           "x-wait-for-model": "true", }
 
 
 def query(payload):
@@ -23,19 +24,19 @@ def query(payload):
     return response.json()
 
 
-text = """Multiple swing states are considering or have passed legislation regulating the use of artificial intelligence in political advertising, a novel medium that is expected to wield growing influence over future elections.
-“Deepfakes” (videos, audio recordings, photos, and other content created through AI to impersonate someone without their consent) have raised concerns over their ability to interfere with elections. Such technology has been used to impersonate President Joe Biden and Taylor Swift, for example.
-Here’s how battleground states are regulating the use of AI in the political arena.
-Swing states considering laws
-Pennsylvania, North Carolina, and Ohio are considering legislation that aims to regulate the use of AI in political elections.
-Several Pennsylvania state representatives have spearheaded AI bills. These proposals include instituting a task force to study the matter, creating a registry of companies developing AI software, requiring disclosures of any AI content, and banning videos using AI-created deepfakes without permission."""
+text = """Since Kamala Harris launched her presidential campaign in late July, fracking has taken on renewed political prominence, particularly given the vice president’s wavering stances and the administration’s efforts to restrict drilling for oil and gas.
+While the vice president and Democratic nominee has vowed not to ban the drilling technology, her stance on fracking, also known as hydraulic fracturing, has changed in recent years. Previously, Harris backed doing away with the technology altogether, while President Joe Biden ran for office aiming to stop new oil and gas drilling on federal lands by halting new leases."""
 sentences = sent_tokenize(text)
 
 data = {"inputs": sentences, }
 tic = time()
 output = query(data)
 toc = time()
-print(output)
+if 'error' in output:
+    print(output)
+    sys.exit(0)
+for sentence in output:
+    print(sentence['translation_text'])
 print('-'*50)
-print(f"{len(sentences)} sentences have been translated {
-      toc-tic:.2f} seconds ({(toc-tic)/len(sentences):.2f} sec/sentence)")
+print(f"{len(sentences)} sentences have been translated " \
+    f"{toc-tic:.2f} seconds ({(toc-tic)/len(sentences):.2f} sec/sentence)")
