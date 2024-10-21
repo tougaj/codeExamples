59a991cbd49fe8e6fe93d1a07ca779ee4a92b494
tougaj <sashamolodec@gmail.com>
2024-10-21 10:23:19 +0300

    Додані приклади виконистання docker

--- qdrant/docker_run.sh
+++ qdrant/docker_run.sh
@@ -2,6 +2,11 @@
 
 # docker pull qdrant/qdrant
 
-docker run -p 6333:6333 -p 6334:6334 \
+# Якщо Вам потрібно, щоб цей контейнер автоматично запускався завжди,
+# додайте до команди запуску "--restart always"
+docker run -d --name qdrant -p 6333:6333 -p 6334:6334 \
     -v $(pwd)/qdrant_storage:/qdrant/storage:z \
     qdrant/qdrant
+
+# Для зупинка даного контейнера використовуйте
+# docker stop qdrant
