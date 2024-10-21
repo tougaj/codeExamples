b41f98d9ff2b3d90867d975cc2fdc440a57981fa
tougaj <sashamolodec@gmail.com>
2024-10-21 12:59:37 +0300

    Доповнення скрипта запуску конвертера

--- qdrant/docker_run.sh
+++ qdrant/docker_run.sh
@@ -11,9 +11,23 @@
 #                   is manually restarted. (See the second bullet listed in restart policy details)
 # unless-stopped	Similar to always, except that when the container is stopped 
 #                   (manually or otherwise), it isn't restarted even after Docker daemon restarts.
+#
+# Крім того, якщо Ви хочете, щоб контейнер автоматично видалявся після остановки,
+# додайте до команди запуску "--rm"
+
 docker run -d --name qdrant -p 6333:6333 -p 6334:6334 \
     -v $(pwd)/qdrant_storage:/qdrant/storage:z \
     qdrant/qdrant
+
+# Якщо Ви використовуєте "--name qdrant", то в подальшому запускати контейнер
+# можна за допомогою команди:
+# docker start qdrant
+#
+# Видалити контейнер можна командою:
+# docker rm qdrant
+#
+# Переглянути список всіх контейнерів можнна командою:
+# docker ps -a
 
 # Для зупинка даного контейнера використовуйте наступну команду:
 # docker stop qdrant
