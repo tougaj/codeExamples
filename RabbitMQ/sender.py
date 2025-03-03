d4528f8a0586edbebc902f395c409cac5871e519
tougaj <sashamolodec@gmail.com>
2025-03-03 15:53:13 +0200

    Уніфікація скриптів Додана можливість під'єднуватись до черги на будь-якому сервері (з використанням відповідних сертифікатів), та задавати ім'я користувача та його пароль.

--- RabbitMQ/sender.py
+++ RabbitMQ/sender.py
@@ -2,6 +2,7 @@
 import os
 import socket
 import ssl
+from pathlib import Path
 from random import randint
 
 import pika
@@ -14,12 +15,15 @@ QUEUE_NAME = 'test'
 HOST = os.getenv('REMOTE_HOST', '127.0.0.1')
 SERVER_HOSTNAME = os.getenv('SERVER_HOSTNAME', socket.gethostname())
 PORT = int(os.getenv('PORT', '5671'))
+CERTIFICATES_ROOT_PATH = Path(os.getenv('CERTIFICATES_ROOT_PATH', './'))
+USER_LOGIN = os.getenv("USER_LOGIN", "test")
+USER_PASSWORD = os.getenv("USER_PASSWORD", "test")
 
 # Налаштування SSL
-context = ssl.create_default_context(cafile="./testca/ca_certificate.pem")
-context.load_cert_chain(certfile="./client/client_certificate.pem", keyfile="./client/private_key.pem")
+context = ssl.create_default_context(cafile=CERTIFICATES_ROOT_PATH / "testca/ca_certificate.pem")
+context.load_cert_chain(certfile=CERTIFICATES_ROOT_PATH / "client/client_certificate.pem", keyfile=CERTIFICATES_ROOT_PATH / "client/private_key.pem")
 
-credentials = pika.PlainCredentials("test", "test")
+credentials = pika.PlainCredentials(USER_LOGIN, USER_PASSWORD)
 # Тут в якості server_hostname необхідно використати вивід команди hostname
 params = pika.ConnectionParameters(
     host=HOST,
