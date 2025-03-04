164edfc4641c64618f0c1c7fc8e2aed1d4d2bae4
tougaj <sashamolodec@gmail.com>
2025-03-04 05:29:49 +0200

    Покращено приклад

--- YouTube/subtitles/get_subtitles.py
+++ YouTube/subtitles/get_subtitles.py
@@ -4,9 +4,11 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from pprint import pprint
 
 def get_subtitles(youtube_id: str):
+	transcript_list = YouTubeTranscriptApi.list_transcripts(youtube_id)
+	print(f"List of available languages:\n{transcript_list}")
 	subtitles = YouTubeTranscriptApi.get_transcript(youtube_id, languages=['uk', 'ru'])
 	pprint(subtitles)
 
 # get_subtitles('Tes245tkUWo')
-# get_subtitles('Jr3Q6Cw92fE')
-get_subtitles('CNA6EFtkO68')
+get_subtitles('Jr3Q6Cw92fE')
+# get_subtitles('CNA6EFtkO68')
