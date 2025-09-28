d1c137857049845a384e6d14409bd246439dffce
tougaj <sashamolodec@gmail.com>
2025-09-28 12:57:01 +0300

    Додаткові налаштування

--- translators/LMStudio/translate.py
+++ translators/LMStudio/translate.py
@@ -12,17 +12,23 @@ lms.configure_default_client(SERVER_API_HOST)
 
 # print(result)
 
+model_key = "google/gemma-3-4b"
+draft_model_key = "google/gemma-3-1b"
+
 
 with lms.Client() as client:
-    model = client.llm.model("google/gemma-3-4b")
+    model = client.llm.model(model_key)
     result = model.respond("""Переклади текст нижче українською мовою. виведи лише переклад. Ось текст:
 Ein Regionaljet mit 53 Passagieren und Crewmitgliedern schoss auf dem Roanoke-Blacksburg Regional Airport im amerikanischen Bundesstaat Virginia über die Landebahn hinaus und kam nicht rechtzeitig zum Stehen. Zement-Zone stoppt Flieger
 Das Flugzeug wurde jedoch erfolgreich durch ein spezielles Sicherheitssystem gestoppt: eine mit Zementblöcken ausgelegte Sicherheitszone. Dadurch wurde ein Unfall verhindert, und es gab keine Verletzten unter den Insassen, berichtet das „Wall Street Journal“.""",
-	config={
-    	"temperature": 0.1,
-	})
+    config={
+        # "draftModel": draft_model_key,
+        # "temperature": 0.1,
+    })
 
     print(result)
+    stats = result.stats
+    print(f"Accepted {stats.accepted_draft_tokens_count}/{stats.predicted_tokens_count} tokens")
 
 # with lms.Client() as client:
 #     model = client.llm.model("google/gemma-3-4b")
