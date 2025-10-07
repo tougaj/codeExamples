a9c1f2a913f6f8922bcc4ca0e7647d9006466a77
tougaj <sashamolodec@gmail.com>
2025-10-07 09:40:43 +0000

    Вдосконалення прикладів

---
+++ vLLM/summarize/common.py
@@ -0,0 +1,59 @@
+texts = [
+    """More than 100 million people, including at least 15 million children, use e-cigarettes, fuelling a new wave of nicotine addiction, the World Health Organization (WHO) is warning.
+Children are, on average, nine times more likely than adults to vape, it says, based on available global figures.
+The WHO's Dr Etienne Krug said e-cigarettes were fuelling a "new wave" of nicotine addiction. "They are marketed as harm reduction but, in reality, are hooking kids on nicotine earlier and risk undermining decades of progress."
+WHO Director General, Dr Tedros, accused the tobacco industry of "agressively targeting" young people.
+"Millions of people are stopping, or not taking up, tobacco use thanks to tobacco control efforts by countries around the world," he said.
+"In response to this strong progress, the tobacco industry is fighting back with new nicotine products, aggressively targeting young people. Governments must act faster and stronger in implementing proven tobacco-control policies," he added.
+The vaping figures are an estimate since some countries - 109 in all, and many in African and South-East Asia - do not gather data.
+According to the report, as of February this year, at least 86 million e-cigarette users were adults, mostly in high-income countries.
+And at least 15 million teenagers aged between 13 and 15 already vape, based on surveys from 123 countries.
+While many nations have made efforts to introduce e-cigarettes regulations to tackle child vaping in recent years, by the end of 2024, 62 countries still had no policy in place, and 74 countries had no minimum age at which e-cigarettes may be purchased, says the WHO.
+Meanwhile, tobacco use has been decreasing - from an estimated 1.38 billion users in 2000 to 1.2 billion in 2024.
+Prevalence of tobacco use among women dropped the most - from 11% in 2010 to 6.6% in 2024.
+Among men, the decrease was from 41.4% in 2010 to 32.5% in 2024.
+But one in five adults globally still uses tobacco.
+Smoking is linked to many diseases, including cancer.
+Experts say vaping is far less harmful than cigarettes, and can help you quit smoking. It is not recommended for non-smokers.
+E-cigarettes do not burn tobacco and do not produce tar or carbon monoxide, two of the most damaging elements in tobacco smoke. They contain nicotine, which can be addictive.""",
+    """Le géant des télécommunications Telefonica, engagé dans un important virage stratégique destiné à accroître sa rentabilité, envisagerait de supprimer 6000 postes, selon la presse espagnole. Une information vivement démentie par le groupe. Selon le quotidien Expansion, Telefonica souhaiterait supprimer «au moins 6000» postes dans le cadre d'un plan de restructuration, une décision qui pourrait toucher «plusieurs filiales» du groupe de télécommunications.
+Une porte-parole de Telefonica a pour sa part assuré que le groupe «travaillait actuellement à de nombreuses analyses dans tous les départements de l'entreprise, mais qu'aucun plan social n'était envisagé pour le moment». L'opérateur historique espagnol est engagé depuis le début d'année dans un important virage stratégique visant à se recentrer sur ses quatre principaux marchés (Espagne, Allemagne, Royaume-Uni et Brésil).
+À lire aussi
+Orange, Telefonica, Deutsche Telekom… Les télécoms européens succombent aux sirènes de Nvidia
+Il a ainsi engagé ces derniers mois la vente de ses filiales en Colombie, en Uruguay et en Équateur, après avoir conclu la cession de ses activités en Argentine et au Pérou. Mais la moins-value constatée lors de la cession de ces filiales a pesé lourd sur les comptes de Telefonica au premier semestre, avec une perte nette atteignant le chiffre astronomique de 1,35 milliard d'euros.
+Telefonica, qui emploie 100.000 personnes dans le monde, avait toutefois confirmé fin juillet ses objectifs qui prévoient une croissance organique pour 2025. Le recentrage engagé depuis le début d'année par l'opérateur espagnol survient à un moment charnière pour le géant des télécoms, au cœur de vastes manœuvres depuis l'entrée surprise de la compagnie saoudienne STC, qui a acquis 9,9% du capital de l'entreprise en septembre 2023. Cette opération avait conduit l'État espagnol à prendre une participation de 10% dans l'entreprise via le fonds public SEPI.
+""",
+    """Україна ще до вступу Дональда Трампа на посаду передавала американській стороні список із близько 200 російських об’єктів, які треба знищити з метою послаблення російських спроможностей на лінії фронту.
+Таку думку висловив військовий аналітик, ветеран російсько-української війни, майор запасу ЗСУ Олексій Гетьман в етері на Радіо NV.
+"Щодо заяв Трампа про те, що він хоче знати, куди саме ми збираємося спрямовувати ці ракети – вочевидь, не по цивільним об’єктам, як це роблять росіяни", - сказав Гетьман.
+Водночас, ще майже рік тому, коли в США президентом був Джо Байден, до Америки приїздила українська делегація і передавала список із 200 об’єктів, які необхідно знищити для того, аби послабити можливості росіян щонайменше на лінії фронту.
+Гетьман вважає заяву Трампа про те, що майже увалив рішення щодо можливості продажу для України ракет Tomahawk "більш популістичною".
+"Він чудово знає куди і що ми збираємося знищувати, які об’єкти є критичними для нас з точки зору російської підтримки керування чи постачання всього того, що відбувається на лінії фронту", - додав Гетьман.
+Він додав, що буде "непогано", якщо ракети "Томагавк" потраплять до України. "Якщо буде, то добре", - зазначив Гетьман.
+Експерт переконаний, що Україна має завдавати ударів по військовим об’єктам на території РФ, і також він впевнений, що Україна буде і далі атакувати енергетичні об’єкти, які живлять військові частини і військові підприємства.
+Як повідомляв УНІАН, президент США Дональд Трамп заявив, що вже наче ухвалив рішення про продаж ракет Tomahawk, але спершу хоче з’ясувати, як українці будуть застосовувати ці ракети.""",
+"""Інспектор "Миколаївського СІЗО" у проросійських Telegram-каналах активно підтримував обстріли Миколаєва, агітував вбивати бійців ЗСУ та навіть закликав РФ обстріляти ізолятор, де сам працював, щоб звільнити зрадників та колаборантів, інформують у Державному бюро розслідувань (ДБР).
+
+"Державне бюро розслідувань системно та послідовно викриває прихильників "руського міра". Поки наші воїни виборюють свободу на фронті, правоохоронці викрили ще одного "ждуна", який працював на окупантів в тилу, намагаючись підірвати нашу оборону зсередини", - повідомляє ДБР в телеграм-каналі у вівторок.
+
+За інформацією Бюро, працівники ДБР у взаємодії з СБУ встановили, що інспектор "Миколаївського СІЗО" у проросійських Telegram-каналах активно підтримував обстріли Миколаєва та дії окупантів загалом.
+
+"Він активно тиражував проросійські гасла, агітував безжально вбивати бійців ЗСУ та навіть закликав рф обстріляти ізолятор, де сам працював, щоб звільнити зрадників та колаборантів, які перебувають під слідством", - зазначається в повідомленні.
+
+Наразі інспектора затримано та повідомлено про підозру у вчиненні кримінальних правопорушень, передбачених ч. 2, ч. 3 ст. 436-2 КК України - виготовлення та поширення матеріалів, у яких міститься виправдовування збройної агресії російської федерації проти України, розпочатої у 2014 році, а також глорифікації осіб, які здійснювали збройну агресію російської федерації проти України, розпочату у 2014 році, вчиненому повторно, ч. 1 ст. 161 КК України - в умисних діях, спрямованих на розпалювання національної ворожнечі, на приниження національної честі та гідності.
+
+Інспектору обрано запобіжний захід тримання під вартою.
+
+Процесуальне керівництво здійснює Миколаївська обласна прокуратура.""",
+"""Wieczór rozpoczął się w eleganckim stylu, ale szybko przerodził się w imprezę, która trwała do wczesnych godzin porannych — event zakończył się dopiero około godz. 3.00. Szczęsny nie stronił od towarzystwa — znalazł czas na papierosa z przyjaciółmi, a także… pokaz swoich umiejętności bramkarskich, tyle że tym razem poza boiskiem.
+
+Wojciech Szczęsny był pewien, że wytną to zdanie z jego filmu. Chodzi o kobiety
+
+Wzruszający moment wydarzył się po koncercie żony bramkarza Mariny, gdy para pocałowała się namiętnie, co uchwyciło wiele kamer i aparatów. Do końca wieczoru Szczęsnemu towarzyszył jego przyjaciel Grzegorz Krychowiak czy kolega z czasów gry w reprezentacji Karol Linetty.
+
+Wśród gości pojawili się także Monika Olejnik z partnerem, Roksana Węgiel z mężem, Quebonafide z partnerką Martyną Byczkowską, czy były selekcjoner reprezentacji Polski Czesław Michniewicz z żoną Grażyną.
+
+Po zakończeniu imprezy na Szczęsnych czekało auto z kierowcą, które zawiozło ich do domu na warszawskim Wilanowie. Niestety, jak donosi świadek, pojazd poruszał się bez włączonych świateł mijania, co zgodnie z przepisami ruchu drogowego może skutkować mandatem w wysokości 300 zł i 4 punktami karnymi. Czy kierowca uniknie konsekwencji? To pozostaje otwarte.
+
+Jedno jest pewne — premiera filmu "Szczęsny" nie tylko odsłoniła nowe oblicze piłkarza, ale także pokazała, że jego życie prywatne i medialne nadal budzi ogromne zainteresowanie.""",
+]
