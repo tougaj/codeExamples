f3b3a33277f3ec344e14d9747b9c4c8a989bb16c
tougaj <sashamolodec@gmail.com>
2025-10-09 13:52:57 +0000

    Вдосконалення скрипта

--- vLLM/summarize/vllm_api_fastAPI_client.py
+++ vLLM/summarize/vllm_api_fastAPI_client.py
@@ -28,32 +28,32 @@ class SummarizationConfig:
 
 
 # 🎯 Готові конфігурації
-CONFIGS = {
-    "precise": SummarizationConfig(
-        temperature=0.2,
-        top_p=0.85,
-        max_tokens=200,
-        presence_penalty=0.6,
-        frequency_penalty=0.4,
-        # stop=["</summary>", "\n\n\n", "Текст:"]
-    ),
-    "balanced": SummarizationConfig(
-        temperature=0.5,
-        top_p=0.9,
-        max_tokens=4196,
-        presence_penalty=0.5,
-        frequency_penalty=0.3,
-        # stop=["</summary>", "\n\n\n"]
-    ),
-    "creative": SummarizationConfig(
-        temperature=0.8,
-        top_p=0.95,
-        max_tokens=600,
-        presence_penalty=0.3,
-        frequency_penalty=0.2,
-        # stop=["</summary>", "\n\n\n"]
-    ),
-}
+# CONFIGS = {
+#     "precise": SummarizationConfig(
+#         temperature=0.2,
+#         top_p=0.85,
+#         max_tokens=200,
+#         presence_penalty=0.6,
+#         frequency_penalty=0.4,
+#         # stop=["</summary>", "\n\n\n", "Текст:"]
+#     ),
+#     "balanced": SummarizationConfig(
+#         temperature=0.5,
+#         top_p=0.9,
+#         max_tokens=4196,
+#         presence_penalty=0.5,
+#         frequency_penalty=0.3,
+#         # stop=["</summary>", "\n\n\n"]
+#     ),
+#     "creative": SummarizationConfig(
+#         temperature=0.8,
+#         top_p=0.95,
+#         max_tokens=600,
+#         presence_penalty=0.3,
+#         frequency_penalty=0.2,
+#         # stop=["</summary>", "\n\n\n"]
+#     ),
+# }
 
 
 def check_health() -> bool:
@@ -70,6 +70,33 @@ def check_health() -> bool:
         return False
 
 
+def print_results(texts: list[str], results: requests.Response, route: str):
+    if results.status_code == 200:
+        data = results.json()
+        for i, (result, text) in enumerate(zip(data['results'], texts), 1):
+            print(f"📄 Текст #{i}: {text[:50]}")
+            print(f"✨ {route}: {result['text']}")
+            print(f"✳️ Причина зупинки: {result['finish_reason']}\n")
+    else:
+        print(f"Помилка: {results.status_code}, {results.text}")
+
+
+def process(texts: list[str], route: str):
+    print(f"{route}\n{'-'*30}")
+    start_time = time.time()
+    results = requests.post(f"{SERVER_ADDRESS}/{route}", json={
+        "texts": texts,
+        # "sampling_params": {
+        #     "temperature": 0.7,
+        #     "top_p": 0.9,
+        #     "max_tokens": 8192
+        # }
+    })
+    end_time = time.time()
+    print_results(texts, results, route)
+    print(f"⏱️ Відповідь зайняла {end_time - start_time:.4f} секунд\n")
+
+
 # 📊 Приклади використання
 if __name__ == "__main__":
     # 🏥 Перевірка здоров'я сервера
@@ -82,32 +109,16 @@ if __name__ == "__main__":
         print("Запусти: uv run fastAPI/main.py")
         exit(1)
 
-    # # Batch-обробка
-    # print("="*50)
-    # print("📦 Тест batch-обробки")
-    # print("="*50 + "\n")
+    # Batch-обробка
+    print("="*30)
+    print("📦 Тест batch-обробки")
+    print("="*30 + "\n")
 
     # batch_texts = news_headlines
-    # # batch_texts = texts
+    batch_texts = texts
 
-    # sample_texts = texts
-    # iteration_count = 1
-    # for i in range(iteration_count):
-    #     print(f"🫧 Iteration {i+1}/{iteration_count}\n{'*'*50}")
-    #     start_time = time.time()
-
-    #     batch_results = client.summarize_batch(
-    #         texts=batch_texts,
-    #         config="balanced",
-    #         show_progress=True
-    #         # custom_instruction=prompt
-    #     )
-    #     end_time = time.time()
-
-    #     # Виведення результатів batch
-    #     for i, result in enumerate(batch_results, 1):
-    #         print(f"📄 Текст #{i}:")
-    #         print(f"✨ Резюме: {result['summary']}\n")
-    #         print(f"✳️ Причина зупинки:")
-    #         print(f"{result['finish_reason']}\n")
-    #     print(f"⏱️ Відповідь зайняла {end_time - start_time:.4f} секунд\n")
+    iteration_count = 1
+    for i in range(iteration_count):
+        print(f"🫧 Iteration {i+1}/{iteration_count}\n{'*'*30}")
+        process(batch_texts, 'translate')
+        process(batch_texts, 'summarize')
