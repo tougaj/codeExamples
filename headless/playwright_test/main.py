6cd3fa5b23c06bcdccbbad618e2dfc692596a5f2
tougaj <sashamolodec@gmail.com>
2025-12-01 13:28:35 +0200

    Покращення прикладу

--- headless/playwright_test/main.py
+++ headless/playwright_test/main.py
@@ -5,27 +5,29 @@ def main():
         browser = p.chromium.launch(headless=False)  # ← показує вікно браузера
         page = browser.new_page()
 
-        # page.goto("https://example.com")
-        # page.goto("https://thehill.com/homenews/administration/5625565-trump-immigration-pause-third-world-countries/", wait_until="networkidle")
-        page.goto("https://thehill.com/homenews/administration/5625565-trump-immigration-pause-third-world-countries/", timeout=30000, wait_until="networkidle")
-        # page.wait_for_timeout(5000)
+        url = "https://thehill.com/homenews/administration/5625565-trump-immigration-pause-third-world-countries/"
         selector = ".article__text"
-        page.wait_for_selector(selector, state="visible", timeout=30000)
-        # page.wait_for_function("selector => !!document.querySelector(selector)", selector)
-        # page.wait_for_timeout(1000)
-        # print(page.title())
+
+        # url = "https://www.nytimes.com/2025/12/01/world/europe/zelensky-ukraine-paris-putin-witkoff.html"
+        # selector = "#story > section[name='articleBody']"
+
+        # url = "https://www.washingtonpost.com/politics/2025/12/01/speechnow-fec-citizens-united-super-pacs/?itid=hp-top-table-main_p001_f002"
+        # selector = ".meteredContent"
+
+        # page.goto(url, timeout=30000, wait_until="networkidle")
+        page.goto(url, timeout=30000, wait_until="domcontentloaded")
+        page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
+        page.locator(selector).wait_for(state="visible")
 
         page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
+        # page.locator('footer').wait_for(state="visible")
         page.wait_for_timeout(1000)  # або краще — чекати конкретний елемент
 
         # html = page.content()
         # print(html)
-        content = page.inner_html(selector)
+        content = page.locator(selector).inner_html()
         # content = page.inner_html("xpath=//div[contains(@class, \"article__text\")]")
         print(content)
-
-        # Браузер залишиться відкритим
-        # input("Натисни Enter, щоб закрити браузер...")
 
         browser.close()
 
